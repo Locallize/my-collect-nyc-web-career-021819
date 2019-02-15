bbeb6ae85fa028ecf72fedc41383867eb43547f9
Fillan Lee <fillanlee@aol.com>
2019-02-15 02:03:52 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+def my_collect (array)
+counter = 0  
+new_array = []
+upcase_array = []
 
+while counter < array.length
+new_array << array[counter].split(" ").first
+# new_array[counter].capi
+upcase_array << array[counter].upcase
+counter += 1
+end
+
+while counter < array.length
+
+
+end
+  new_array
+  upcase_array
+  array
+end
 
