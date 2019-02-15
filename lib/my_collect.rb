3c52e00f9c5605c5e830ffb7050ffa54982024c7
Fillan Lee <fillanlee@aol.com>
2019-02-15 02:24:31 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,8 +4,8 @@ new_array = []
 upcase_array = []
 
 while counter < array.length
- yield new_array << array[counter].split(" ").first
-
+ new_array << array[counter].split(" ").first
+yield new_array
 counter += 1
 end
 
