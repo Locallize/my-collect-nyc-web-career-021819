af331d0561dfb70500bce976a9e480aae3710d3c
Fillan Lee <fillanlee@aol.com>
2019-02-15 02:29:29 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,13 @@
 def my_collect (array)
 counter = 0  
 new_array = []
-upcase_array = []
+
 
 while counter < array.length
 new_array << array[counter].split(" ").first
 
 counter += 1
 end
-yield new_array
+new_array
 
 end
